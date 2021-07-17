1ad969dca03e605a4fedf351dc2d9d8b9d35fbc1
Lucas Seijo <lucaseijo7@gmail.com>
2021-07-17 19:04:41 -0300

    verificacion de email y control de errores agregados

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const btnEnviar = document.querySelector('#enviar');
 const email = document.querySelector('#email');
 const asunto = document.querySelector('#asunto');
 const mensaje = document.querySelector('#mensaje');
-
+const formulario = document.querySelector('#enviar-mail');
 
 eventListeners();
 function eventListeners(){
@@ -29,8 +29,54 @@ function iniciarApp(){
 
 function validarForm(e){
     if(e.target.value.length > 0){
+        // elimina error
+        const error = document.querySelector('p.error');
+        if (error !== null) {
+            error.remove();
+          }
 
+        e.target.classList.remove('border', 'border-red-500');
+        e.target.classList.add('border', 'border-green-500');
     } else {
+        e.target.classList.remove('border', 'border-green-500');
         e.target.classList.add('border', 'border-red-500');
+        
+        mostrarError('Todos los campos son obligatorios');
+    }
+
+    if (e.target.type === 'email'){
+        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (er.test(e.target.value)){
+            const error = document.querySelector('p.error');
+            if (error !== null) {
+                error.remove();
+              }
+
+            e.target.classList.remove('border', 'border-red-500');
+            e.target.classList.add('border', 'border-green-500');
+            
+        } else {
+            e.target.classList.remove('border', 'border-green-500');
+            e.target.classList.add('border', 'border-red-500');
+            
+            mostrarError('El email ingresado es inválido');
+        }
     }
 }
+
+function mostrarError(mensaje){
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = mensaje;
+    mensajeError.classList.add('border', 'border-red-500', 'background-color-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
+
+    const errores = document.querySelectorAll('.error');
+    if (errores.length === 0){
+        formulario.appendChild(mensajeError);
+    }
+
+    
+}
+
+
+
